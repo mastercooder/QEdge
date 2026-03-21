91b243565987e054051499a30d7f0d0c9cc41889
mastercooder <tejesh.patel256@gmail.com>
2026-03-22 03:18:19 +0530

    Learning React

---
+++ React/app1/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Comp1 from './comp1';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>React Application</h3>
+      </header>
+      <h3>Welcome</h3>
+      <Comp1></Comp1>
+    </div>
+  );
+}
+
+ export default Comp1; // ---> use this or || export default || before funtion.
---
+++ React/app1/src/comp1.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Comp1 extends Component{
+    render(){
+        return (
+            <div>
+                <h3>Componenet-1</ h3>
+                <h3>Componenet-1</h3>
+            </div>
+        );
+    }
+}
+
+export default Comp1;
