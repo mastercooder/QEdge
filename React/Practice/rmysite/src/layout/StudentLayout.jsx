17d72e8b2450fcefa96ae9e189c4ec35ef9255d0
mastercooder <tejesh.patel256@gmail.com>
2026-04-09 23:04:21 +0530

    9th Apr Pracitce Code Added

--- React/Practice/rmysite/src/layout/StudentLayout.jsx
+++ React/Practice/rmysite/src/layout/StudentLayout.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 const StudentLayout = () => {
   return (
-    <div>StudentLayout</div>
+    <div style={{display: 'flex', minHeight: '100vh'}}>
+      <h3>StudentLayout Component</h3>
+    </div>
   )
 }
 
--- React/Practice/rmysite/src/routes/AppRoutes.jsx
+++ React/Practice/rmysite/src/routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+
 import Login from '../pages/auth/Login';
 import SignUp from '../pages/auth/SignUp';
 import CourseGallery from '../pages/CourseGallery';
