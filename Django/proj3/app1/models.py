fe4411e16771cbcf50fdd1a51684908bc6f27a55
mastercooder <tejesh.patel256@gmail.com>
2026-07-21 15:11:14 +0530

    Class Attended: 7 May 2026 || Code & Notes Added

--- Django/proj3/app1/models.py
+++ Django/proj3/app1/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class student:
+    def __init__(self, sid, sname, course, fee):
+        pass
+    
+student1 = student(101, 'ravi', 'python', 12000)
