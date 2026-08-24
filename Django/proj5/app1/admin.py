00a26cab799061ae4967d6bb68e9bdb80666c7ad
mastercooder <tejesh.patel256@gmail.com>
2026-08-24 15:12:53 +0530

    Class Attended: 11th May 2026 || Code & Notes Added

--- Django/proj5/app1/admin.py
+++ Django/proj5/app1/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from app1.models import student 
+from app1.models import student2
 
 # Register your models here.
+
+# admin.site.register(student)
+
+@admin.register(student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'sid', 'sname', 'email', 'course']
+    
+@admin.register(student2)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'sid', 'sname', 'email', 'contact', 'create_at', 'joined_at', 'course']
--- Django/proj5/app1/models.py
+++ Django/proj5/app1/models.py
@@ -3,8 +3,23 @@ from django.db import models
 # Create your models here.
 
 class student(models.Model):
-    def __init__(self):
-        sid = models.IntegerField()
-        sname = models.CharField(max_length=20)
-        email = models.EmailField(max_length=35)
-        course = models.CharField(max_length=20)
+    sid = models.IntegerField()
+    sname = models.CharField(max_length=20)
+    email = models.EmailField(max_length=35)
+    course = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.sname
+    
+
+class student2(models.Model):
+    sid = models.IntegerField()
+    sname = models.CharField(max_length=20)
+    email = models.EmailField(max_length=35)
+    contact = models.IntegerField(max_length=10)
+    create_at = models.DateTimeField()
+    joined_at = models.DateField()
+    course = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.sname
