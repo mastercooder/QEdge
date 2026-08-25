ab78dcd04956c7aa4d298ce530ddd3fd457277f2
mastercooder <tejesh.patel256@gmail.com>
2026-08-25 18:56:32 +0530

    Class Attended: 12th May 2026 || Code Added

--- Django/proj5/app1/admin.py
+++ Django/proj5/app1/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from app1.models import student 
 from app1.models import student2
+from app1.models import student3
 
 # Register your models here.
 
@@ -13,3 +14,8 @@ class StudentAdmin(admin.ModelAdmin):
 @admin.register(student2)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['id', 'sid', 'sname', 'email', 'contact', 'create_at', 'joined_at', 'course']
+    
+    
+@admin.register(student3)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['sid', 'firstName', 'lastName', 'email', 'phoneNumber', 'dob', 'loginTime', 'signUpAt', 'modifiedAt', 'studentStatus', 'projectURL', 'aboutMe']
--- Django/proj5/app1/models.py
+++ Django/proj5/app1/models.py
@@ -23,3 +23,21 @@ class student2(models.Model):
     
     def __str__(self):
         return self.sname
+    
+    
+class student3(models.Model):
+    sid = models.BigAutoField(primary_key=True)
+    firstName = models.CharField(max_length=15)
+    lastName = models.CharField(max_length=15)
+    email = models.EmailField(max_length=35)
+    phoneNumber = models.CharField(max_length=10)
+    dob = models.DateField()
+    loginTime = models.TimeField()
+    signUpAt = models.DateTimeField(auto_now_add=True)
+    modifiedAt = models.DateTimeField(auto_now=True)
+    studentStatus = models.BooleanField(default=True)
+    projectURL = models.URLField(max_length=200)
+    aboutMe = models.TextField(max_length=500)
+    
+    def __str__(self):
+        return self.firstName + " " + self.lastName 
